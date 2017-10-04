97efdfcd5a24b27c4d830238a1e353534d8a2735
mryozer <ryan@bw-family.com>
2017-10-03 17:56:52 -0700

    im an idiot

---
+++ Team4042/src/main/java/org/firstinspires/ftc/team4042/UltrasonicI2cRangeSensor.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.team4042;
+
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchDevice;
+
+/**
+ * Created by meme on 10/3/2017.
+ */
+
+public class UltrasonicI2cRangeSensor extends I2cDeviceSynchDevice {
+}
