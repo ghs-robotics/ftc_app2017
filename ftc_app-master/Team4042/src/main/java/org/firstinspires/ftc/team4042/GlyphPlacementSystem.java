97add18db0097dceb8374ea621141166d763d92a
trixiecatsrule <confirmationemailconfirmer@gmail.com>
2017-11-02 17:03:49 -0700

    Hand servo

--- ftc_app-master/Team4042/src/main/java/org/firstinspires/ftc/team4042/GlyphPlacementSystem.java
+++ ftc_app-master/Team4042/src/main/java/org/firstinspires/ftc/team4042/GlyphPlacementSystem.java
@@ -130,7 +130,7 @@ public class GlyphPlacementSystem
             verticalDrive.setTargetPosition(BLOCK_DISP * (targetY + 1));
 
             if( (currentY.ordinal() - 2 == targetY || override) && timer.milliseconds() >= 1000) {
-                grabbyBoi.setPosition(0);
+                grabbyBoi.setPosition(.57);
                 goToHome();
                 isPlacing = false;
             }
