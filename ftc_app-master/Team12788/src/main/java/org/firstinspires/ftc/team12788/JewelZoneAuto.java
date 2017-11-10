33746ce85ee956cf7a59c37053a388bb71d326b6
gpsanant <gpsanant@gmail.com>
2017-11-10 11:36:45 -0800

    auto

--- ftc_app-master/Team12788/src/main/java/org/firstinspires/ftc/team12788/JewelZoneAuto.java
+++ ftc_app-master/Team12788/src/main/java/org/firstinspires/ftc/team12788/JewelZoneAuto.java
@@ -5,13 +5,15 @@ import android.util.Log;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.Servo;
 
-<<<<<<< HEAD
+
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
-=======
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.Rotation;
 import org.lasarobotics.vision.android.Cameras;
 import org.lasarobotics.vision.opmode.LinearVisionOpMode;
 import org.lasarobotics.vision.opmode.extensions.CameraControlExtension;
@@ -28,11 +30,11 @@ import org.opencv.imgproc.Imgproc;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.HashMap;
->>>>>>> e3409f252d79ba5be186247f68afaf2e1a4a69ac
 
 
-@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Zone Only", group="Linear Opmode")
-public class JewelZoneAuto extends LinearVisionOpMode {
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Zone Onl", group="Linear Opmode")
+public class JewelZoneAuto extends LinearOpMode {
 
     private MecanumDrive drive = new MecanumDrive();
     private VuMarkIdentifier mark = new VuMarkIdentifier();
@@ -45,99 +47,84 @@ public class JewelZoneAuto extends LinearVisionOpMode {
     public Servo grabLeft;
     public Servo grabRight;
     public Servo jewel;
-    private RelicRecoveryVuMark vuMark;
+
+    ColorSensor color = new ColorSensor("color");
+
+
 
     DigitalSensor whisker = new DigitalSensor("whisker");
 
     private RelicRecoveryVuMark vuMark;
 
-    public String getBallColor(Mat frame){
-        telemetry.addData("size", frame.height() + " x " + frame.width());
-        Imgproc.resize(frame, frame, new Size(960, 720));
-        telemetry.update();
-        Rect left_crop = new Rect(new Point(215,585), new Point(380, 719));
-        Rect right_crop = new Rect(new Point(460,585), new Point(620, 719));
-
-        Log.d("stupid", this.getFrameSize().width + " x " + this.getFrameSize().height);
-        Mat right = new Mat(frame, right_crop);
-        Mat left = new Mat(frame, left_crop);
 
 
-        String result = "unspecified";
-        Scalar left_colors  = Core.sumElems(left);
-        Scalar right_colors = Core.sumElems(right);
-
-        if(left_colors.val[0] >= left_colors.val[2]){
-            result = "red";
-        } else {
-            result = "blue";
-        }
-
-        if(right_colors.val[0] >= right_colors.val[2]){
-            result = result.concat(", red");
-        } else {
-            result = result.concat(", blue");
-        }
-
-        return result;
-    }
-
-    public boolean isBallRight() {
-        try {
-            String balls = getBallColor(getFrameRgba());
-            discardFrame();
-            telemetry.addData("ball orientation", balls);
-            switch (balls) {
-                case "red":
-                    return false;
-                case "blue":
-                    return true;
-                case "red, blue":
-                    return false;
-                case "blue, red":
-                    return true;
-                case ", blue":
-                    return false;
-                case ", red":
-                    return true;
-            }
-        } catch (CvException ex) {
-            StringWriter sw = new StringWriter();
-            ex.printStackTrace(new PrintWriter(sw));
-            telemetry.addData("CvException", sw.toString());
-        }
-        return true;
-    }
-
-    public void waitMili(int time) {
-        try {
-            sleep(time);
-        } catch (InterruptedException ex) { }
-    }
 
     @Override
     public void runOpMode() {
-    drive.initialize(telemetry, hardwareMap);
-    //liftLeft = hardwareMap.dcMotor.get("liftLeft");
-    //liftRight = hardwareMap.dcMotor.get("liftRight");
-    //intakeLeft = hardwareMap.dcMotor.get("intakeLeft");
-    //intakeRight = hardwareMap.dcMotor.get("intakeRight");
-    drive.runWithoutEncoders();
-    //grabLeft = hardwareMap.servo.get("grabLeft");
-    //grabRight = hardwareMap.servo.get("grabRight");
-    //mark.initialize(telemetry, hardwareMap);
-    waitForStart();}
+        drive.initialize(telemetry, hardwareMap);
+        //liftLeft = hardwareMap.dcMotor.get("liftLeft");
+        //liftRight = hardwareMap.dcMotor.get("liftRight");
+        //intakeLeft = hardwareMap.dcMotor.get("intakeLeft");
+        //intakeRight = hardwareMap.dcMotor.get("intakeRight");
+        drive.runWithoutEncoders();
+        //grabLeft = hardwareMap.servo.get("grabLeft");
+        //grabRight = hardwareMap.servo.get("grabRight");
+        //mark.initialize(telemetry, hardwareMap);
+        //waitForStart();
+    }
+    public void reset(){
+        drive.resetEncoders();
+        sleep(500);
+    }
+
+    public void move(Direction direction, double speed, double targetTicks){
+        while(drive.driveWithEncoders(direction, speed, targetTicks) && opModeIsActive())
+        reset();
+    }
+
+    public void rotate(Direction.Rotation rotation, double speed, double targetTicks){
+        while(drive.rotateWithEncoders(rotation, speed, targetTicks) && opModeIsActive())
+            reset();
+    }
 
 
+    public void dropoff() {
+        while (!drive.driveWithEncoders(Direction.Backward, Autonomous.speedy, 9 * Autonomous.tile / 24) && opModeIsActive()) ;
+        grabLeft.setPosition(.5);
+        grabRight.setPosition(-.1);
+        while (!drive.driveWithEncoders(Direction.Forward, Autonomous.speedy, 9 * Autonomous.tile / 24) && opModeIsActive())
+            ;
+    }
 
+    public void jewelKnock(boolean isRed){
+        NormalizedRGBA colors = color.JewelColor();
+        jewel.setPosition(-1);
+        if (isRed){
+            if (color.SenseRed()){
+                move(Direction.Forward, Autonomous.speedy, Autonomous.tile);
 
-
-        public void dropoff(){
-            while (!drive.driveWithEncoders(Direction.Backward, Autonomous.speedy, 9 * Autonomous.tile / 24) && opModeIsActive()) ;
-            grabLeft.setPosition(.5);
-            grabRight.setPosition(-.1);
-            while (!drive.driveWithEncoders(Direction.Forward , Autonomous.speedy, 9 * Autonomous.tile / 24) && opModeIsActive()) ;
+            }
+            else{
+                move(Direction.Backward, Autonomous.speedy, Autonomous.tile/2);
+                jewel.setPosition(1);
+                move(Direction.Forward, Autonomous.speedy, 3*Autonomous.tile/2);
+            }
         }
+        else{
+            if (color.SenseBlue()){
+                move(Direction.Forward, Autonomous.speedy, Autonomous.tile/2);
+                jewel.setPosition(1);
+                move(Direction.Backward, Autonomous.speedy, Autonomous.tile);
+            }
+            else{
+                move(Direction.Backward, Autonomous.speedy, Autonomous.tile/2);
+            }
+        }
+        jewel.setPosition(-1);
+    }
+
+
+
 
 
 
@@ -148,114 +135,122 @@ public class JewelZoneAuto extends LinearVisionOpMode {
         //intakeLeft = hardwareMap.dcMotor.get("intakeLeft");
         //intakeRight = hardwareMap.dcMotor.get("intakeRight");
         drive.runWithoutEncoders();
+        if(gamepad1.x){
+            isRed = false;
+        }
+        if(gamepad1.b){
+            isRed = true;
+        }
+        if(gamepad1.y){
+            isTop = true;
+        }
+        if(gamepad1.a){
+            isTop = false;
+        }
+
 
         //grabLeft = hardwareMap.servo.get("grabLeft");
         //grabRight = hardwareMap.servo.get("grabRight");
         //mark.initialize(telemetry, hardwareMap);
-<<<<<<< HEAD
-        waitForStart();
+
+        // waitForStart();
+        jewel.setPosition(-1);
         vuMark = mark.getMark();
         if (isRed && !isTop) {
-
+            jewelKnock(isRed);
             while (!whisker.getState()) {
                 drive.driveXYR(.7, 0, 1, 0);
-                while ((drive.rotateWithEncoders(Direction.Rotation.Clockwise,Autonomous.speedy-.2,Autonomous.turn)) && opModeIsActive());
+                rotate(Direction.Rotation.Counterclockwise,Autonomous.speedy-.2,Autonomous.turn);
             }
+            jewel.setPosition(1);
 
             if (vuMark == RelicRecoveryVuMark.LEFT) {
-                while (drive.driveWithEncoders(Direction.Left, Autonomous.speedy,1*Autonomous.tile/6) && opModeIsActive());
+                move(Direction.Right, Autonomous.speedy,1*Autonomous.tile/6);
                 dropoff();
+                move(Direction.Right, Autonomous.speedy,1*Autonomous.tile/3);
             }
             if (vuMark == RelicRecoveryVuMark.CENTER) {
-                while (drive.driveWithEncoders(Direction.Left, Autonomous.speedy,3*Autonomous.tile/6) && opModeIsActive());
+                move(Direction.Right, Autonomous.speedy,3*Autonomous.tile/6);
                 dropoff();
             }
             if (vuMark == RelicRecoveryVuMark.RIGHT) {
-                while (drive.driveWithEncoders(Direction.Left, Autonomous.speedy,5*Autonomous.tile/6) && opModeIsActive());
+                move(Direction.Right, Autonomous.speedy,5*Autonomous.tile/6);
                 dropoff();
+                move(Direction.Left, Autonomous.speedy,1*Autonomous.tile/3);
             }
 
 
         }
         if (!isRed && !isTop) {
+            jewelKnock(!isRed);
             while (!whisker.getState()) {
-                drive.driveXYR(.7, 0, 1, 0);
-                while ((drive.rotateWithEncoders(Direction.Rotation.Counterclockwise, Autonomous.speedy - .2, Autonomous.turn)) && opModeIsActive()) ;
+                drive.driveXYR(.7, 0, -1, 0);
+                rotate(Direction.Rotation.Counterclockwise, Autonomous.speedy - .2, Autonomous.turn) ;
             }
+            jewel.setPosition(1);
                 if (vuMark == RelicRecoveryVuMark.LEFT) {
-                    while (drive.driveWithEncoders(Direction.Right, Autonomous.speedy,1*Autonomous.tile/6) && opModeIsActive());
+                    move(Direction.Left, Autonomous.speedy,1*Autonomous.tile/6);
                     dropoff();
+                    move(Direction.Left, Autonomous.speedy,1*Autonomous.tile/3);
                 }
                 if (vuMark == RelicRecoveryVuMark.CENTER) {
-                    while (drive.driveWithEncoders(Direction.Right, Autonomous.speedy,3*Autonomous.tile/6) && opModeIsActive());
+                    move(Direction.Left, Autonomous.speedy,3*Autonomous.tile/6);
                     dropoff();
                 }
                 if (vuMark == RelicRecoveryVuMark.RIGHT) {
-                    while (drive.driveWithEncoders(Direction.Right, Autonomous.speedy,5*Autonomous.tile/6) && opModeIsActive());
+                    move(Direction.Left, Autonomous.speedy,5*Autonomous.tile/6);
                     dropoff();
+                    move(Direction.Right, Autonomous.speedy,1*Autonomous.tile/3);
                 }
             }
 
         if (isRed && isTop) {
+            jewelKnock(isRed);
             while (!whisker.getState()) {
-                drive.driveXYR(.7, 0, 1, 0);
+                drive.driveXYR(.7, 0, 0, -1);
             }
+            jewel.setPosition(1);
+            rotate(Direction.Rotation.Clockwise,Autonomous.speedy-.2,Autonomous.turn);
             if (vuMark == RelicRecoveryVuMark.LEFT) {
-                while (drive.driveWithEncoders(Direction.Left, Autonomous.speedy,5*Autonomous.tile/6) && opModeIsActive());
+                move(Direction.Right, Autonomous.speedy,5*Autonomous.tile/6);
                 dropoff();
+                move(Direction.Left, Autonomous.speedy,1*Autonomous.tile/3);
             }
             if (vuMark == RelicRecoveryVuMark.CENTER) {
-                while (drive.driveWithEncoders(Direction.Left, Autonomous.speedy,3*Autonomous.tile/6) && opModeIsActive());
+                move(Direction.Right, Autonomous.speedy,3*Autonomous.tile/6);
                 dropoff();
             }
             if (vuMark == RelicRecoveryVuMark.RIGHT) {
-                while (drive.driveWithEncoders(Direction.Left, Autonomous.speedy,1*Autonomous.tile/6) && opModeIsActive());
+                move(Direction.Right, Autonomous.speedy,1*Autonomous.tile/6);
                 dropoff();
+                move(Direction.Right, Autonomous.speedy,1*Autonomous.tile/3);
             }
         }
         if (!isRed && isTop) {
+            jewelKnock(!isRed);
             while (!whisker.getState()) {
-                drive.driveXYR(.7, 0, 1, 0);
+                drive.driveXYR(.7, 0, 0, 1);
             }
+            jewel.setPosition(1);
+            rotate(Direction.Rotation.Counterclockwise,Autonomous.speedy-.2,Autonomous.turn);
             if (vuMark == RelicRecoveryVuMark.LEFT) {
-                while (drive.driveWithEncoders(Direction.Right, Autonomous.speedy,1*Autonomous.tile/6) && opModeIsActive());
+                move(Direction.Left, Autonomous.speedy,1*Autonomous.tile/6);
                 dropoff();
+                move(Direction.Left, Autonomous.speedy,1*Autonomous.tile/3);
             }
             if (vuMark == RelicRecoveryVuMark.CENTER) {
-                while (drive.driveWithEncoders(Direction.Right, Autonomous.speedy,3*Autonomous.tile/6) && opModeIsActive());
+                move(Direction.Left, Autonomous.speedy,3*Autonomous.tile/6);
                 dropoff();
             }
             if (vuMark == RelicRecoveryVuMark.RIGHT) {
-                while (drive.driveWithEncoders(Direction.Right, Autonomous.speedy,5*Autonomous.tile/6) && opModeIsActive());
+                move(Direction.Left, Autonomous.speedy,5*Autonomous.tile/6);
                 dropoff();
+                move(Direction.Right, Autonomous.speedy,1*Autonomous.tile/3);
             }
         }
 
 
 
-
-
-
-
-=======
-
-        this.setCamera(Cameras.PRIMARY);
-        this.setFrameSize(new Size(900, 900));
-        //enableExtension(Extensions.BEACON);
-        enableExtension(Extensions.ROTATION);
-        enableExtension(Extensions.CAMERA_CONTROL);
-        rotation.setIsUsingSecondaryCamera(false);
-        rotation.disableAutoRotate();
-        rotation.setActivityOrientationFixed(ScreenOrientation.LANDSCAPE);
-        cameraControl.setColorTemperature(CameraControlExtension.ColorTemperature.AUTO);
-        cameraControl.setAutoExposureCompensation();
-
-        try {
-            waitForStart();
-        } catch (InterruptedException ex) { }
-        while(!drive.driveWithEncoders(Direction.Forward, Autonomous.speedy-.1, 1.25 * Autonomous.tile) && opModeIsActive());
-        waitMili(2000);
->>>>>>> e3409f252d79ba5be186247f68afaf2e1a4a69ac
 
     }
 }
