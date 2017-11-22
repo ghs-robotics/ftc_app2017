0d47c37c6ad4f416e50572d6db6055de760cfce6
trixiecatsrule <confirmationemailconfirmer@gmail.com>
2017-11-21 16:20:40 -0800

    Reorganized the file structure

---
+++ ftc_app-master/Team4042/src/main/java/org/firstinspires/ftc/team4042/testops/AutoEncoderTest.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.team4042.testops;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.team4042.drive.Direction;
+import org.firstinspires.ftc.team4042.drive.Drive;
+import org.firstinspires.ftc.team4042.drive.MecanumDrive;
+
+@Autonomous(name="EncoderTest", group="testops")
+public class AutoEncoderTest extends LinearOpMode {
+
+    MecanumDrive drive = new MecanumDrive(true);
+
+    @Override
+    public void runOpMode() {
+        drive.initialize(telemetry, hardwareMap);
+        //drive.setUseGyro(true);
+
+        waitForStart();
+
+        drive.setEncoders(true);
+        autoDrive(Direction.Forward, Drive.FULL_SPEED, 1000000);
+        //check sensor sums
+        //robot starts facing right
+        //scan vision patter
+        //go to front of jewels
+        //cv scan
+        //knock off other jewel
+        //head right
+        //whisker sensor hits cryptobox
+        //back up
+        //repeat ^ until whisker disengages
+        //move right until we see -^-^-| from ultrasonic
+        //place block
+        //detach and extend robot towards glyph
+    }
+
+    /**
+     * Drives in the given Direction at the given speed until targetTicks is reached
+     * @param direction The direction to head in
+     * @param speed The speed to move at
+     * @param targetTicks The final distance to have travelled, in encoder ticks
+     */
+    private void autoDrive(Direction direction, double speed, double targetTicks) {
+        boolean done = false;
+        while (opModeIsActive() && !done) {
+            done = drive.driveWithEncoders(direction, speed, targetTicks);
+            telemetry.update();
+        }
+    }
+}
