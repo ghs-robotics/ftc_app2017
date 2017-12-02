a2ae4f9313f2e5debcb083306a15205a03040119
mryozer <ryan@bw-family.com>
2017-12-01 18:33:20 -0800

    fixed placer again

--- ftc_app-master/Team4042/src/main/java/org/firstinspires/ftc/team4042/drive/GlyphPlacementSystem.java
+++ ftc_app-master/Team4042/src/main/java/org/firstinspires/ftc/team4042/drive/GlyphPlacementSystem.java
@@ -143,7 +143,7 @@ public class GlyphPlacementSystem {
     }
 
     public void runToPosition() {
-        drive.setVerticalDrive((drive.verticalDriveTargetPos() - drive.verticalDriveCurrPos())/ 100);
+        drive.setVerticalDrive((drive.verticalDriveTargetPos() - drive.verticalDriveCurrPos())/ 20);
 
         int pos = drive.verticalDriveCurrPos();
 
