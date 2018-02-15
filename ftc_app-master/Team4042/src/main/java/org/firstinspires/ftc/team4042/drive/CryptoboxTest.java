7b208c657bc8bfe5573677305a6bfeb53562aedc
trixiecatsrule <confirmationemailconfirmer@gmail.com>
2018-02-15 09:41:08 -0800

    Added a cryptobox test class that doesn't require a rev hub

---
+++ ftc_app-master/Team4042/src/main/java/org/firstinspires/ftc/team4042/drive/CryptoboxTest.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.team4042.drive;
+
+import android.hardware.Camera;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.team4042.autos.C;
+
+@TeleOp(name = "CryptoboxTest", group="drive")
+public class CryptoboxTest extends OpMode {
+
+    private Cryptobox cryptobox;
+    private boolean bA;
+    private boolean bX;
+
+    @Override
+    public void init() {
+        cryptobox = new Cryptobox(telemetry);
+    }
+
+    @Override
+    public void start() {
+    }
+    
+    @Override
+    public void loop() {
+        if (!bA && gamepad2.a) {
+            cryptobox.placeGlyph(Cryptobox.GlyphColor.BROWN);
+        }
+        if (!bX && gamepad2.x) {
+            cryptobox.placeGlyph(Cryptobox.GlyphColor.GREY);
+        }
+        bA = gamepad2.a;
+        bX = gamepad2.x;
+    }
+
+    @Override
+    public void stop() {
+    }
+}
