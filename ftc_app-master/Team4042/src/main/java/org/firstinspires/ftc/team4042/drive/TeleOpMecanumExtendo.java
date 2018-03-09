e41a708aca542eefb00833672a2aff8692746b48
trixiecatsrule <confirmationemailconfirmer@gmail.com>
2018-03-09 08:52:08 -0800

    Added extendo-starting mecanum classes

---
+++ ftc_app-master/Team4042/src/main/java/org/firstinspires/ftc/team4042/drive/TeleOpMecanumExtendo.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.team4042.drive;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by Hazel on 2/18/2018.
+ */
+@TeleOp(name = "Mecanum Extendo", group="drive")
+public class TeleOpMecanumExtendo extends TeleOpMecanum {
+    @Override
+    public void init() {
+        super.init();
+        super.drive.toggleServoExtendo();
+    }
+}
---
+++ ftc_app-master/Team4042/src/main/java/org/firstinspires/ftc/team4042/drive/TeleOpMecanumExtendoCryptoboxClear.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.team4042.drive;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by Hazel on 2/18/2018.
+ */
+@TeleOp(name = "Mecanum Extendo CRYPTOBOX CLEAR", group="drive")
+public class TeleOpMecanumExtendoCryptoboxClear extends TeleOpMecanum {
+    @Override
+    public void init() {
+        super.init();
+        super.drive.cryptobox.clear();
+        super.drive.cryptobox.writeFile();
+        super.drive.toggleServoExtendo();
+    }
+}
